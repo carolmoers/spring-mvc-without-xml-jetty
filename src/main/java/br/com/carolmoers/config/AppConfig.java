c98473f5f81c01388e9a7505a2028dc5151aa8f8
Caroline Moers <Caroline_Moers@gap.com>
2016-09-13 13:32:19 -0300

    Initial commit

---
+++ src/main/java/br/com/carolmoers/config/AppConfig.java
@@ -0,0 +1,9 @@
+package br.com.carolmoers.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "br.com.carolmoers")
+class AppConfig {
+}
---
+++ src/main/java/br/com/carolmoers/controller/HelloController.java
@@ -0,0 +1,17 @@
+package br.com.carolmoers.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+class HelloController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @ResponseBody
+    public String hello() {
+        return "Hello world";
+    }
+
+}
